ea15b6d33b63abc4e5205df3d8a10ac351d3601d
ArnabMukherjee1991 <mukherjee.arnab1991@gmail.com>
2019-03-21 00:59:00 +0530

    Updated Message

--- Springdemo/src/main/java/com/javabrains/springdemo/Triangle.java
+++ Springdemo/src/main/java/com/javabrains/springdemo/Triangle.java
@@ -4,7 +4,7 @@ public class Triangle {
 
 	public void draw() {
 
-		System.out.println("This is a Triangle " + this.getClass().getSimpleName());
+		System.out.println("This is a New Triangle " + this.getClass().getSimpleName());
 	}
 
 }
